1ed99a7f2bb95c740bcc9cf82af972a22a2c094f
Arbitermartin <arbitermartin04@gmail.com>
2026-06-19 14:35:48 +0300

    add changes

---
+++ server.js
@@ -0,0 +1,52 @@
+/* ******************************************
+ * This server.js file is the primary file of the 
+ * application. It is used to control the project.
+ *******************************************/
+/* ***********************
+ * Require Statements
+ *************************/
+const express = require("express")
+const env = require("dotenv").config()
+const expressLayouts = require("express-ejs-layouts")
+const app = express()
+// const baseController = require("./controllers/baseController")
+const static = require("./routes/static")
+
+
+/**************************
+ * View Engine and Template
+ *********************
+ */
+app.set("view engine","ejs")
+app.use(expressLayouts)
+app.set("layout","./layouts/layout")
+
+
+/* ***********************
+ * Routes
+ *************************/
+app.use(static)
+
+/**********************
+ * build home view
+ */
+//index route
+app.get("/", function(req, res){
+  res.render("index", {title: "Home"})
+})
+// app.get("/",baseController.buildHome)
+
+
+/* ***********************
+ * Local Server Information
+ * Values from .env (environment) file
+ *************************/
+const port = process.env.PORT
+const host = process.env.HOST
+
+/* ***********************
+ * Log statement to confirm server operation
+ *************************/
+app.listen(port, () => {
+  console.log(`app listening on ${host}:${port}`)
+})
